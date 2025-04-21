eeb22e8be9bc8f236e08e7fd04237f9b2b72e281
JazzStone2 <jazzstone880@gmail.com>
2025-04-20 23:06:16 -0700

    more ui and items

--- Assets/Script/CraftedItem.cs
+++ Assets/Script/CraftedItem.cs
@@ -6,4 +6,5 @@ public class CraftingRecipe : ScriptableObject
     public Item craftedItem; // The item to be crafted
     public Item[] requiredMaterials; // Array of required materials
     public int[] requiredAmounts; // Corresponding quantities for each material
+    public int AmountGiven;
 }
